8857d3f8a737e3d0a23d938252a4cf8626949a54
play4fun <ddwrt.fantasia@gmail.com>
2014-02-23 01:54:18 +0800

    Update ath9k_watchdog_4_ddwrt.sh
    do a wireless scan if necessary.

--- ath9k_watchdog_4_ddwrt.sh
+++ ath9k_watchdog_4_ddwrt.sh
@@ -1,10 +1,14 @@
-#!/bin/sh 
+#!/bin/sh
+INTERFACE=`nvram get wl0_ifname`
 tail -F -n 1 /var/log/messages | while read line 
-do 
+do
    case "$line" in 
    *"Failed to stop TX DMA"*) 
          #Restart lan service(including wirelss service) 
          startservice_f lan 
+      ;;   
+   *"disconnected due to excessive missing ACKs"*) 
+         iw dev ${INTERFACE} scan
       ;; 
    *) 
       ;; 
